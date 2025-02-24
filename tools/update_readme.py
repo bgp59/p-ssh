b9d397e594ff345798fdb6db277b0b5b2719b787
Emy Parparita <emy.parparita@gmail.com>
2025-02-24 15:14:10 -0800

    in-memory README update

--- tools/update_readme.py
+++ tools/update_readme.py
@@ -13,23 +13,26 @@ sys.path.insert(0, os.path.join(root_dir, "src"))
 
 import p_ssh
 
-work_dir = os.path.join(root_dir, ".work")
-os.makedirs(work_dir, exist_ok=True)
-
 readme_file = os.path.join(root_dir, "README.md")
-tmp_readme_file = os.path.join(work_dir, "README.md")
 
-with open(readme_file, "rt") as r, open(tmp_readme_file, "wt") as w:
-    download_url_updated = False
+updated = False
+in_memory = []
+with open(readme_file, "rt") as r:
     for line in r:
-        if not download_url_updated:
-            m = re.match(
-                r"""(.*https://github.com/.+/releases/download/v)[^/]+(/[^-]+-)[^-]+(-(?s:.)+)""",
-                line,
-            )
-            if m is not None:
-                line = p_ssh.__version__.join(m.groups())
-                download_url_updated = True
-        w.write(line)
+        m = re.match(
+            r"""(.*https://github.com/.+/releases/download/v)[^/]*(/[^-]+-)[^-]+(-(?s:.)+)""",
+            line,
+        )
+        if m is not None:
+            new_line = p_ssh.__version__.join(m.groups())
+            if new_line != line:
+                updated = True
+                in_memory.append(new_line)
+                updated = True
+            continue
+        in_memory.append(line)
 
-os.rename(tmp_readme_file, readme_file)
+if updated:
+    with open(readme_file, "wt") as w:
+        for line in in_memory:
+            w.write(line)
